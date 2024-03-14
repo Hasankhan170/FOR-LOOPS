6da34e7541e30cd0151abff96601bfaaad4258eb
Hasankhan170 <hasankhanali0316@gmail.com>
2024-03-13 19:37:22 -0700

    index file

---
+++ app.js
@@ -0,0 +1,109 @@
+// Q.1 
+// for(let i = 1; i <= 5; i++){
+//     console.log("hello world", i)
+// }
+// done 
+
+// Q.2
+
+// for(let i=1 ; i <= 10 ;i++){
+//    console.log(i)
+// }
+// done 
+
+// Q.3
+
+// const userInput = +prompt("enter a table number")
+// const times = +prompt("length of multiple table")
+
+// for(let i = 1; i <= times ; i++){
+//     console.log(`${userInput}*${i} = ${userInput*i}`)
+// }
+// done 
+
+// Q.4
+
+// const phones = ["MOTOROLA" , "NOKIA" , "Q MOBILE" , "IPHONE"]
+
+// for(let i = 1 ; i <= 1 ; i++){
+//     document.write(phones[0])
+//     document.write("<br>")
+//     document.write(phones[1])
+//     document.write("<br>")
+//     document.write(phones[2])
+//     document.write("<br>")
+//     document.write(phones[3])
+// }
+
+// done 
+
+// Q.5
+
+// const fruits = ["banana" , " " , "mango" ,  " " , "apple" ,  " " , "grapes" ,  " " , "strawberry"]
+
+// for(let i = 0 ; i < fruits.length ; i++){
+//     document.write(fruits[i])
+// }
+// done 
+
+// Q.6
+
+// const N = +prompt("enter a number")
+// const items = []
+
+// for(let i=0; i<N ; i++){
+//     const item = prompt("Enter items" + (i+1) + ':')
+//     items.push(item)
+// }
+// document.write(items)
+
+// done 
+
+
+// Q.7
+
+// const counting = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
+// document.write("Counting :")
+// for(let i= 1; i<= 15 ; i++){
+//     document.write(i + ", ")
+// }
+
+
+// document.write("<br>")
+// document.write("<br>")
+
+
+// const reverse = (10,9,8,7,6,5,4,3,2,1)
+// document.write("Reverse Counting :")
+// for(let j = 10 ; j >= 1 ; j--){
+//     document.write(j + ",")
+// }
+
+// document.write("<br>")
+// document.write("<br>")
+
+// const Even = (0,2,4,6,8,10,12,14,16,18,20)
+// document.write("Even :")
+// for(let k = 0 ; k <= 20 ; k+=2){
+//     document.write(k + ",")
+// }
+
+// document.write("<br>")
+// document.write("<br>")
+
+// const odd = (1,3,5,7,9,11,13,15,17,19)
+// document.write("Odd :")
+// for(let l = 1 ; l <= 19 ; l+=2){
+//     document.write(l + ",")
+// }
+
+// document.write("<br>")
+// document.write("<br>")
+
+// const series = (2,4,6,8,10,12,14,16,18,20)
+// document.write("Series :")
+// for(let m = 2 ; m <= 20 ; m+=2){
+//     document.write(m + "k,")
+// }
+
+// done 
